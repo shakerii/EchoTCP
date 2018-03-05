abb4f99aa23080daa10cf74e02d72b6fc65427e3
Ramtin <r.shakeri1375@gmail.com>
2018-03-05 13:31:06 +0330

    last updates

--- client.go
+++ client.go
@@ -8,14 +8,15 @@ import (
 )
 
 func main() {
-	conn, _ := net.Dial(
-		"tcp", "127.0.0.1:5000")
+	conn, _ := net.Dial(Protocol, Address)
 	for {
-		reader := bufio.NewReader(os.Stdin) //?
-		fmt.Print("Type : ")
-		text, err := reader.ReadString('\n')
-		fmt.Println(err)
-		fmt.Println(conn, text + "\n")
+		// read in input from stdin
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+		// send to socket
+		fmt.Fprintf(conn, text+"\n")
+		// listen for reply
 		message, _ := bufio.NewReader(conn).ReadString('\n')
 		fmt.Print("Message from server: " + message)
 	}
--- main.go
+++ main.go
@@ -6,16 +6,29 @@ import (
 	"fmt"
 )
 
-const host = "localhost"
-const port = "5000"
+const (
+	Address = "127.0.0.1:5000"
+	Protocol = "tcp"
+)
 
 func main() {
-	ln, _ := net.Listen("tcp", host + ":" + port)
+	fmt.Println("Launching server...")
+
+	// listen on all interfaces
+	ln, _ := net.Listen(Protocol, Address)
+
+	// accept connection on port
 	conn, _ := ln.Accept()
+
+	// run loop forever (or until ctrl-c)
 	for {
+		// will listen for message to process ending in newline (\n)
 		message, _ := bufio.NewReader(conn).ReadString('\n')
+		// output message received
 		fmt.Print("Message Received:", string(message))
-		newMessage := message
-		conn.Write([]byte(newMessage + "\n"))
+		// sample process for string received
+		// newmessage := strings.ToUpper(message)
+		// send new string back to client
+		conn.Write([]byte(message + "\n"))
 	}
 }
