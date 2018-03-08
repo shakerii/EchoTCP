942e328afc189bf44d8ce2f03070f6c1451860df
Ramtin <r.shakeri1375@gmail.com>
2018-03-08 05:57:04 +0330

    not completely handle Ctrl+C in server

--- server.go
+++ server.go
@@ -5,18 +5,35 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/golang/EchoTCP/constants"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	connList := make([]net.Conn, 0)
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		for _, conn := range connList {
+			conn.Write([]byte("bye\n"))
+			conn.Close()
+		}
+		os.Exit(0)
+	}()
+
 	ln, _ := net.Listen(constants.Protocol, constants.Address)
 	i := 0
 	for {
 		conn, _ := ln.Accept()
+		connList = append(connList, conn)
 		i++
-		go func (conn net.Conn, i int) {
+		go func(conn net.Conn, i int) {
 			for {
 				if message, err := bufio.NewReader(conn).ReadString('\n'); err != nil {
 					fmt.Println("End Connection!")
+					conn.Close()
 					break
 				} else {
 					fmt.Print("Message(", i, ") : ", string(message))
